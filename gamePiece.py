11a1f989dd7d77ba9b507c062e4f588f527fabc6
Jordan <rainonej@gmail.com>
2020-03-31 02:22:31 -0400

    Added __repr__, __str__, and quick_init functions to the GamePiece class and all subclasses
    We can now just read the data from an object by hitting enter. And we can also use the print function.
    
    There is a new class function quick_init which takes in the output of repr(piece) as either a string or a dictionary. It returns a new instance of the class with the correct pieceType.
    
    You can use this function when testing to different intances quickly and easily.
    
    The function also inherits to the subclasses in a smart way

--- gamePiece.py
+++ gamePiece.py
@@ -7,7 +7,7 @@ class GamePiece:
         This class represents a single piece on the game board.  
         It possesses general methods and attributes which any piece will need.
     '''
-    
+
     def __init__(self, xLocation, yLocation):
         self.location = np.array([xLocation,yLocation])
         self.inCommunication = True;
@@ -24,7 +24,90 @@ class GamePiece:
         
         self.threatenedValue = 0;
         self.supportedValue = 0; 
+
+    '''Functions to help with representation of objects'''
+
+    def __repr__(self):
+        "Should return a string representation of the class or instance with as much information as possible, preferably something that can be passed to eval/quick_init to recreate the object. Return value must be a string." 
+        "Only returns the attributes that different from the default ones. If you want to see all values, then use self.__dict__"
+
+        #Decide which class to compare our piece to
+        pieceType = self.pieceType
+        if (pieceType == 'I'):
+            test_piece = Infantry(0,0)
+        elif (pieceType == 'H'):
+            test_piece = Cavalry(0,0)
+        elif (pieceType == 'A'):
+            test_piece = FootArtillery(0,0)
+        elif (pieceType == 'A_H'):
+            test_piece = SwiftArtillery(0,0)
+        elif (pieceType == 'C'):
+            test_piece = FootRelay(0,0)
+        elif (pieceType == 'C_H'):
+            test_piece = SwiftRelay(0,0)
+        else:
+            test_piece = GamePiece(0,0)
+
+        test_dic = test_piece.__dict__
+        key_list = list(test_dic)
+        key_list.remove('location') # always want location displayed
+        key_list.remove('pieceType') # always want piece type displayed
+
+        dic = self.__dict__.copy()
+        dic['location'] = tuple(dic['location']) # Can't have location being displayed as an array or else it causes problems for the quick_init function
+
+        for i in range(0,len(key_list)):
+
+            if (dic[key_list[i]] == test_dic[key_list[i]]):
+                del dic[key_list[i]]
+
+        return str(dic)
+
+    def __str__(self):
+        "A user friendly text representation of the instance. Allows the 'print' to work without giving too much information."
+        "Only gives the peiceType and the Location information"
+
+        dic = {'pieceType': self.pieceType, 'location':tuple(self.location)}
+        return str(dic)
+
+    def quick_init(dicORstr):
+        "Takes in the output of __repr__ either as a string or as a dictionary. Returns a new member of the class with this information"
+        if (type(dicORstr) == str):
+            dic = eval(dicORstr)
+        else:
+            dic = dicORstr
+
+        key_list = list(dic)
+        key_list.remove('location')
+
+        #Decide where the piece belongs
+        pieceType = dic['pieceType']
+        if (pieceType == 'I'):
+            piece = Infantry(dic['location'][0],dic['location'][1])
+        elif (pieceType == 'H'):
+            piece = Cavalry(dic['location'][0],dic['location'][1])
+        elif (pieceType == 'A'):
+            piece = FootArtillery(dic['location'][0],dic['location'][1])
+        elif (pieceType == 'A_H'):
+            piece = SwiftArtillery(dic['location'][0],dic['location'][1])
+        elif (pieceType == 'C'):
+            pieceType = FootRelay(dic['location'][0],dic['location'][1])
+        elif (pieceType == 'C_H'):
+            piece = SwiftRelay(dic['location'][0],dic['location'][1])
+        else:
+            piece = GamePiece(dic['location'][0],dic['location'][1])
+
+        #Put all the different attributes where they belong
+        for i in range(0,len(key_list)):
+
+            # using piece.key_list[i] didn't work
+            setattr(piece,key_list[i],dic[key_list[i]]) 
+        return piece
+       
+    
           
+    '''Actual class methods'''
+
     def movementRange(self): 
         '''
             A function which returns a list of all possible location coordinates that can be reached by this piece. 
@@ -73,6 +156,7 @@ class Infantry(GamePiece):
         
         self.threatenedValue = 0;
         self.supportedValue = 0; 
+
         
         
 class Cavalry(GamePiece):
@@ -204,53 +288,16 @@ class SwiftRelay(Relay):
         
         
         
+#Just so I can run some tests quickly
+a = GamePiece(10,10)
+b = GamePiece(9,8)
+b.moveRange = 4
+c =  GamePiece.quick_init("{'location': (9, 7), 'pieceType': 'Fake Piece', 'moveRange': 9}")
+d = GamePiece.quick_init({'location': (-5, 0), 'pieceType': '100',  'threatenedValue':1})
+e = GamePiece.quick_init({'location': (30, 0), 'pieceType': 'C_H',  'extraValue':"easter egg"})
+f = Infantry.quick_init("{'location': (9, 7), 'pieceType': 'I', 'moveRange': -9}")
+       
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
         
         
